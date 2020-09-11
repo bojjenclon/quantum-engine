38ddce5236e94399ba6ec70fc0ad12d8e11ee024
Cornell Daly <dalycornell@gmail.com>
2020-09-11 17:43:18 -0400

    Remove test code

--- Sources/quantum/QuantumEngine.hx
+++ Sources/quantum/QuantumEngine.hx
@@ -78,59 +78,12 @@ class QuantumEngine
 
 		onResize(System.windowWidth(0), System.windowHeight(0));
 
-		sprite = new Sprite("tex");
-		sprite.x = 64;
-		sprite.y = 32;
-		sprite.scale.x = 4;
-		sprite.scale.y = 2;
-		sprite.alpha = 0.7;
-		sprite.color = Color.Red;
-		sprite.immobile = true;
-		sprite.addCollider(Polygon.rectangle(0, 0, sprite.width, sprite.height, true));
-
-		sub = new Sprite("tex");
-		sub.x = 64;
-		sub.y = 96;
-		sub.alpha = 0.5;
-		sprite.addChild(sub);
-		sub.addCollider(Polygon.rectangle(0, 0, sub.width, sub.height, true));
-
-		anim = new AnimatedSprite("player", 32, 32);
-		anim.x = 128;
-		anim.y = 256;
-
-		anim.addAnimation("idle", [0], 0, false);
-		anim.addAnimation("run", [1, 2, 3, 4], 0.15, true);
-		anim.addAnimation("jump", [5, 6, 7, 8, 9], 0.15, false);
-		anim.addAnimation("air", [10], 0, false);
-		anim.play("run");
-
-		var s2 = new Sprite("tex");
-		s2.x = 128;
-		s2.y = 60;
-		s2.scale.x = 3;
-		s2.scale.y = 2;
-		s2.color = Color.Blue;
-		s2.addCollider(Polygon.rectangle(0, 0, s2.width, s2.height, true));
-
-		scene = new Scene();
-		scene.background = 0xff009999;
-		scene.addChild(sprite);
-		scene.addChild(anim);
-		scene.addChild(s2);
-
 		var input = Input.instance;
 		input.initialize();
+
+		#if debug
 		input.register("debugMenu", KeyCode.BackQuote);
-		input.register("exit", KeyCode.Escape);
-		input.register("left", KeyCode.Left);
-		input.register("left", KeyCode.A);
-		input.register("right", KeyCode.Right);
-		input.register("right", KeyCode.D);
-		input.register("up", KeyCode.Up);
-		input.register("up", KeyCode.W);
-		input.register("down", KeyCode.Down);
-		input.register("down", KeyCode.S);
+		#end
 
 		// Doesn't currently work for all targets, so
 		// for now we use a hack.
@@ -213,25 +166,6 @@ class QuantumEngine
 				debugUI.visible = !debugUI.visible;
 			}
 			#end
-
-			if (input.isDown("left"))
-			{
-				// sub.rotation--;
-				sub.x--;
-			}
-			else if (input.isDown("right"))
-			{
-				// sub.rotation++;
-				sub.x++;
-			}
-			if (input.isDown("up"))
-			{
-				sub.y--;
-			}
-			else if (input.isDown("down"))
-			{
-				sub.y++;
-			}
 
 			if (scene != null)
 			{
