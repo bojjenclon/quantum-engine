e848b145476d84daaa8788390d87bb44420c206a
Cornell Daly <dalycornell@gmail.com>
2020-09-13 13:31:50 -0400

    Fix Tiled loading, initial rendering of map

---
+++ Sources/quantum/entities/display/TiledMapEntity.hx
@@ -0,0 +1,130 @@
+package quantum.entities.display;
+
+import kha.Assets;
+import kha.graphics2.Graphics;
+import kha.Image;
+import quantum.tiled.TiledTileSet;
+import quantum.tiled.TiledTileLayer;
+import quantum.tiled.TiledMap;
+import quantum.partials.IRenderable;
+
+private typedef RenderableTile =
+{
+	var image : Image;
+	var x : Int;
+	var y : Int;
+	var tilesetX : Int;
+	var tilesetY : Int;
+	var tileWidth : Int;
+	var tileHeight : Int;
+}
+
+private typedef RenderableTileLayer =
+{
+	var tiles : Array<RenderableTile>;
+}
+
+class TiledMapEntity extends Entity
+{
+	var _tiledMap : TiledMap;
+
+	var _tileLayers : Array<TiledTileLayer> = [];
+
+	var _renderableLayers : Array<RenderableTileLayer> = [];
+
+	static final ASSET_PATH_REGEX : EReg = ~/[\s.,\/\\]/g;
+
+	public function new(map : TiledMap, rootPath : String = "")
+	{
+		super();
+
+		_tiledMap = map;
+
+		for (layer in map.layers)
+		{
+			if (Std.is(layer, TiledTileLayer))
+			{
+				_tileLayers.push(cast(layer, TiledTileLayer));
+			}
+		}
+
+		for (layer in _tileLayers)
+		{
+			var layerWidth = layer.width;
+			var layerHeight = layer.height;
+			
+			var x = 0;
+			var y = 0;
+
+			var tilesToRender : Array<RenderableTile> = [];
+
+			for (tile in layer.tiles)
+			{
+				if (tile != null)
+				{
+					var tileset : TiledTileSet = getTileset(tile.tilesetID);
+					var assetName = getTilesetImagePath(tileset, rootPath);
+					var tileId = tile.tileID - tileset.firstGID;
+
+					tilesToRender.push({
+						image: Assets.images.get(assetName),
+
+						x: x * tileset.tileWidth,
+						y: y * tileset.tileHeight,
+
+						tilesetX: Std.int(tileId % tileset.numCols) * tileset.tileWidth,
+						tilesetY: Std.int(tileId / tileset.numCols) * tileset.tileHeight,
+
+						tileWidth: tileset.tileWidth,
+						tileHeight: tileset.tileHeight
+					});
+				}
+
+				x++;
+				if (x >= layerWidth)
+				{
+					x = 0;
+					y++;
+				}
+			}
+
+			_renderableLayers.push({
+				tiles: tilesToRender
+			});
+		}
+	}
+
+	override public function render(g : Graphics)
+	{
+		for (layer in _renderableLayers)
+		{
+			for (tile in layer.tiles)
+			{
+				g.drawSubImage(tile.image, tile.x, tile.y, tile.tilesetX, tile.tilesetY, tile.tileWidth, tile.tileHeight);
+			}
+		}
+	}
+
+	function getTileset(gid : Int) : TiledTileSet
+	{
+		for (tileset in _tiledMap.tilesets)
+		{
+			if (tileset.hasGid(gid))
+			{
+				return tileset;
+			}
+		}
+
+		return null;
+	}
+
+	function getTilesetImagePath(tileset : TiledTileSet, rootPath : String = "") : String
+	{
+		var imageSource = tileset.imageSource;
+		var extStart = imageSource.indexOf(".");
+		var cleanImagePath = ASSET_PATH_REGEX.replace(imageSource, "_");
+		cleanImagePath = cleanImagePath.substring(0, extStart);
+
+		return '${rootPath}${cleanImagePath}';
+	}
+}
--- Sources/quantum/tiled/TiledTileLayer.hx
+++ Sources/quantum/tiled/TiledTileLayer.hx
@@ -8,13 +8,15 @@ import kha.internal.BytesBlob;
 import haxe.xml.Access;
 import quantum.tiled.TiledLayer.TiledLayerType;
 
+using StringTools;
+
 class TiledTileLayer extends TiledLayer
 {
 	public var x : Int;
 	public var y : Int;
 	public var width : Int;
 	public var height : Int;
-	public var tiles : Array<TiledTile>;
+	public var tiles(get, null) : Array<TiledTile>;
 
 	public var encoding(get, null) : String;
 	public var csvData(get, null) : String;
@@ -23,6 +25,7 @@ class TiledTileLayer extends TiledLayer
 	var xmlData : Access;
 
 	static inline var BASE64_CHARS : String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+	static final WHITESPACE_REGEX : EReg = ~/\s/g;
 
 	public function new(source : Access, parent : TiledMap)
 	{
@@ -33,14 +36,61 @@ class TiledTileLayer extends TiledLayer
 		width = Std.parseInt(source.att.width);
 		height = Std.parseInt(source.att.height);
 
-		tiles = new Array<TiledTile>();
-
 		// load tile GIDs
 		xmlData = source.node.data;
 		if (xmlData == null)
 		{
 			throw "Error loading TiledLayer level data";
 		}
+	}
+
+	function base64ToByteArray(data : String) : Bytes
+	{
+		var output : Bytes = Bytes.alloc(Std.int(data.length / 4 * 3));
+
+		// initialize lookup table
+		var lookup : Array<Int> = new Array<Int>();
+
+		for (c in 0...BASE64_CHARS.length)
+		{
+			lookup[BASE64_CHARS.charCodeAt(c)] = c;
+		}
+
+		var i : Int = 0;
+		var outputPosition : Int = 0;
+		while (i < data.length - 3)
+		{
+			// Ignore whitespace
+			if (data.charAt(i) == " " || data.charAt(i) == "\n" || data.charAt(i) == "\r")
+			{
+				i++;
+				continue;
+			}
+
+			// read 4 bytes and look them up in the table
+			var a0 : Int = lookup[data.charCodeAt(i)];
+			var a1 : Int = lookup[data.charCodeAt(i + 1)];
+			var a2 : Int = lookup[data.charCodeAt(i + 2)];
+			var a3 : Int = lookup[data.charCodeAt(i + 3)];
+
+			// convert to and write 3 bytes
+			if (a1 < 64)
+			{
+				output.set(outputPosition++, (a0 << 2) + ((a1 & 0x30) >> 4));
+			}
+			if (a2 < 64)
+			{
+				output.set(outputPosition++, ((a1 & 0x0f) << 4) + ((a2 & 0x3c) >> 2));
+			}
+			if (a3 < 64)
+			{
+				output.set(outputPosition++, ((a2 & 0x03) << 6) + a3);
+			}
+
+			i += 4;
+		}
+
+		return output;
 	}
 
 	function getByteArrayData() : Bytes
@@ -50,9 +100,10 @@ class TiledTileLayer extends TiledLayer
 		if (encoding == "base64")
 		{
 			var chunk : String = xmlData.innerData;
+			var trimmedChunk = WHITESPACE_REGEX.replace(chunk, '');
+			
 			var compressed : Bool = false;
-
-			result = Base64.decode(chunk);
+			result = base64ToByteArray(chunk);
 
 			if (xmlData.has.compression)
 			{
@@ -120,44 +171,61 @@ class TiledTileLayer extends TiledLayer
 		return csvData;
 	}
 
+	function generateTilesData()
+	{
+		tiles = new Array<TiledTile>();
+		tileArray = new Array<Int>();
+
+		if (encoding == "csv")
+		{
+			var endline : String = csvData.indexOf("\r\n") != -1 ? "\r\n" : "\n";
+			var rows : Array<String> = csvData.split(endline);
+
+			for (row in rows)
+			{
+				var cells : Array<String> = row.split(",");
+				for (cell in cells)
+				{
+					if (cell != "")
+					{
+						tileArray.push(Std.parseInt(cell));
+					}
+				}
+			}
+		}
+		else
+		{
+			var mapData : Bytes = getByteArrayData();
+
+			if (mapData == null)
+			{
+				throw "Must use Base64 encoding (with or without zlip compression) in order to get 1D Array.";
+			}
+
+			var i = 0;
+			while (i < mapData.length)
+			{
+				tileArray.push(resolveTile(mapData.getInt32(i)));
+				i += 4;
+			}
+		}
+	}
+
+	function get_tiles() : Array<TiledTile>
+	{
+		if (tiles == null)
+		{
+			generateTilesData();
+		}
+
+		return tiles;
+	}
+
 	function get_tileArray() : Array<Int>
 	{
 		if (tileArray == null)
 		{
-			tileArray = new Array<Int>();
-
-			if (encoding == "csv")
-			{
-				var endline : String = csvData.indexOf("\r\n") != -1 ? "\r\n" : "\n";
-				var rows : Array<String> = csvData.split(endline);
-
-				for (row in rows)
-				{
-					var cells : Array<String> = row.split(",");
-					for (cell in cells)
-					{
-						if (cell != "")
-						{
-							tileArray.push(Std.parseInt(cell));
-						}
-					}
-				}
-			}
-			else
-			{
-				var mapData : Bytes = getByteArrayData();
-
-				if (mapData == null)
-				{
-					throw "Must use Base64 encoding (with or without zlip compression) in order to get 1D Array.";
-				}
-
-				var i = 0;
-				while (i < Std.int(mapData.length))
-				{
-					tileArray.push(resolveTile(mapData.getInt32(i)));
-				}
-			}
+			generateTilesData();
 		}
 
 		return tileArray;
